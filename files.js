06426ce4e794340760c34ce4d1613c293c8dd456
Amoy <flyoverrs@outlook.com>
2022-06-04 14:30:42 +0800

    1

--- files.js
+++ files.js
@@ -2,21 +2,16 @@
 
 
 
-const codeFilename = "RS TEST"
-const gitHubUrl = "https://raw.githubusercontent.com/mzeryck/Weather-Cal/main/weather-cal-code.js"
+function InitWidget(){
 
-// Determine if the user is using iCloud.
-let files = FileManager.local()
-const iCloudInUse = files.isFileStoredIniCloud(module.filename)
+    const widget = new ListWidget()
+    widget.backgroundColor = Color.green()
 
-// If so, use an iCloud file manager.
-files = iCloudInUse ? FileManager.iCloud() : files
+    const one = widget.addStack()
+    one.addText("one")
 
-// Determine if the Weather Cal code exists and download if needed.
-const pathToCode = files.joinPath(files.documentsDirectory(), codeFilename + ".js")
-if (!files.fileExists(pathToCode)) {
-  console.log('文件不存在，开始下载生成')
-  const req = new Request(gitHubUrl)
-  const codeString = await req.loadString()
-  files.writeString(pathToCode, codeString)
+    const two = widget.addStack()
+    two.addText("two")
+
+    return widget
 }
